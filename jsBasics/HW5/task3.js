cad5d7aeb00e1ed7614053ac4478c6d2c9c76471
AlexKhytko <xitkoalex@gmail.com>
2024-10-13 12:19:24 +0300

    hw5

---
+++ jsBasics/HW5/task3.js
@@ -0,0 +1,14 @@
+const number = 845745744;
+
+for (let i=1; i<=10; i++) {
+let multipNumber = number * i;
+console.log(number, " x ", i, " = ", multipNumber)
+}
+
+let j = 1;
+do {let multipNumber = number * j;
+    console.log(number, " x ", j, " = ", multipNumber);
+
+    ;
+   }
+   while(j <= 10);
